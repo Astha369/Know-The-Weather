b9c3729022eb9484c9e76bbfb1275c1eccf65bcd
Astha Tripathi <79215705+Astha369@users.noreply.github.com>
2023-07-11 22:09:22 +0530

    Create apiKeys.js

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "5b112e8e3880e60d3c24fbe0efab9805",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
